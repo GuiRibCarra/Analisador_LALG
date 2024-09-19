afb34225d4e7ef7b75bd67ae4781ae41ee3f6802
GuiRibCarra <guilherme.carrara@unesp.br>
2024-09-19 14:18:57 -0300

    inicio sintatico program/variaveis

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import tkinter.filedialog
 import tkinter.messagebox
 from tkinter import *
+
+#------------------------------------------ARRAYS ELEMENTOS-------------------------------------------------------------
+alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_']
+alfabeto_m = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+numeros=['0','1','2','3','4','5','6','7','8','9']
+reservadas= ['program','procedure','var','int','float','boolean','read','write','true','false','begin','end','if','then','else','while','do','or','div','and','not']
+simbolos = ['::=',':=',':','=','<','>','<=','>=',',','<>','+','-','*','[',']','{','}',';','.','_','(',')','/']
+lexemas = []
+i_lexemas = 0
+
+
 #------------------------------------------------ FUNCOES -------------------------------------------------------------
 def inicia_tab():
     tabela.insert(END,'ELEMENTO')
@@ -9,6 +20,8 @@ def inicia_tab():
     tabela2.itemconfig(END, {'bg': '#EE82EE'})
     tabela3.insert(END, 'LINHA')
     tabela3.itemconfig(END, {'bg': '#EE82EE'})
+    sintatico.insert(END,'ERROS SINTATICOS')
+    sintatico.itemconfig(END, {'bg': '#EE82EE'})
 
 def ler_comentario(texto,posicao):
     i = posicao+1
@@ -36,6 +49,7 @@ def abrir_arq():
     tabela.delete(0,END)
     tabela2.delete(0, END)
     tabela3.delete(0, END)
+    sintatico.delete(0, END)
     inicia_tab()
     arq = tkinter.filedialog.askopenfilename()
 
@@ -66,12 +80,15 @@ def ler_texto(texto,posicao):
             i = i+1
     if palavra in reservadas:
         tipo = 'PALAVRA RESERVADA '+palavra.upper()
+        lexemas.append(tipo)
         return palavra,i,tipo
     elif len(palavra) > limite:
         tipo = 'IDENTIFICADOR FORA DO LIMITE'
+        lexemas.append('IDENTIFICADOR')
         return palavra,i,tipo
     else:
         tipo = 'IDENTIFICADOR'
+        lexemas.append(tipo)
         return palavra, i, tipo
 
 def numeros_I_R(texto,posicao):
@@ -79,6 +96,7 @@ def numeros_I_R(texto,posicao):
   if posicao == tamanho:
       numero = texto[posicao]
       tipo = 'NUMERO_INTEIRO'
+      lexemas.append(tipo)
       return numero,posicao,tipo
 
   tamanho_max = 15
@@ -104,13 +122,17 @@ def numeros_I_R(texto,posicao):
           tamanho_c = tamanho_c + 1
         if tamanho_c > tamanho_max or tamanho_n > tamanho_max:
             tipo = 'NUMERO MAIOR QUE O LIMITE'
+            lexemas.append('Numero')
             return numero, k, tipo
         tipo = 'NUMERO_REAL'
+        lexemas.append(tipo)
         return numero,k,tipo
       if tamanho_n > tamanho_max:
           tipo = 'NUMERO MAIOR QUE O LIMITE'
+          lexemas.append('Numero')
           return numero, j, tipo
       tipo = 'NUMERO_INTEIRO'
+      lexemas.append(tipo)
       return numero,j,tipo
 
 def especiais(texto,posicao):
@@ -120,100 +142,126 @@ def especiais(texto,posicao):
       if texto[i] == ':':
           if texto[i+1]+texto[i+2] == ':=':
               tipo = '2P_2P_IGUAL'
-              especi = '<='
+              especi = '::='
+              lexemas.append(tipo)
               return especi, i+1, tipo
           elif texto[i+1] == '=':
               tipo = '2P_IGUAL'
               especi = ':='
+              lexemas.append(tipo)
               return especi, i+1, tipo
           else:
               tipo = '2PONTOS'
               especi = ':'
+              lexemas.append(tipo)
               return especi, i, tipo
       if texto[i] == '<':
           if texto[i+1] == '=':
               tipo = 'MENOR_IGUAL'
               especi = '<='
+              lexemas.append(tipo)
               return especi, i+1, tipo
           elif texto[i+1] == '>':
               tipo = 'MENOR_MAIOR'
               especi = '<>'
+              lexemas.append(tipo)
               return especi, i+1, tipo
           else:
               tipo = 'MENOR'
               especi = '<'
+              lexemas.append(tipo)
               return especi, i, tipo
       if texto[i] == '>':
           if texto[i+1] == '=':
               tipo = 'MAIOR_IGUAL'
               especi = '>='
+              lexemas.append(tipo)
               return especi, i+1, tipo
           else:
               tipo = 'MAIOR'
               especi = '>'
+              lexemas.append(tipo)
               return especi, i, tipo
       if texto[i] == '(':
         tipo = 'ABRE_PARENTESES'
         especi = '('
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == ')':
         tipo = 'FECHA_PARENTESES'
         especi = ')'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '[':
         tipo = 'ABRE_COLCHETES'
         especi = '['
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == ']':
         tipo = 'FECHA_COLCHETES'
         especi = ']'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '{':
         tipo = 'ABRE_CHAVES'
         especi = '{'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '}':
         tipo = 'FECHA_CHAVES'
         especi = '}'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '+':
         tipo = 'OP_SOMA'
         especi = '+'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '-':
         tipo = 'OP_SUBTRACAO'
         especi = '-'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '*':
         tipo = 'OP_MULTIPLICACAO'
         especi = '*'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '=':
         tipo = 'OP_IGUAL'
         especi = '='
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '.':
         tipo = 'OP_PONTO'
         especi = '.'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == ';':
         tipo = 'OP_PONTO_VIRGULA'
         especi = ';'
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == ',':
         tipo = 'OP_VIRGULA'
         especi = ','
+        lexemas.append(tipo)
         return especi,i,tipo
       if texto[i] == '/':
         tipo = 'OP_BARRA'
         especi = '/'
+        lexemas.append(tipo)
         return especi,i,tipo
   return None, None, None
+
+def limpar_sintatico():
+    sintatico.delete(0,END)
 
 def imprimirAnalise():
     tabela.delete(0,END)
     tabela2.delete(0, END)
     tabela3.delete(0, END)
+    limpar_sintatico()
     inicia_tab()
     texto = bloco.get('1.0',END)
     terminou = False
@@ -335,6 +383,12 @@ def imprimirAnalise():
                 tabela.itemconfig(END, {'bg': '#00FF7F'})
                 tabela2.itemconfig(END, {'bg': '#00FF7F'})
                 tabela3.itemconfig(END, {'bg': '#00FF7F'})
+    lexemas.append('$')
+    for i in range(len(lexemas)):
+        print(lexemas[i])
+    index = 0
+    program(index)
+    lexemas.clear()
 
 def atualiza_linha(event):
     texto = bloco.get('0.0',END)
@@ -353,6 +407,7 @@ def multiple_yview(*args):
     tabela.yview(*args)
     tabela2.yview(*args)
     tabela3.yview(*args)
+    sintatico.yview(*args)
 
 def multiple_yview_l(*args):
     bloco.yview(*args)
@@ -361,19 +416,96 @@ def multiple_yview_l(*args):
 def scrollwheel(event):
     return 'break'
 
-#------------------------------------------ARRAYS ELEMENTOS-------------------------------------------------------------
+def obter_token(i):
+    texto = lexemas[i]
+    index = i + 1
+    print(texto,i)
+    return texto,index
 
-alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_']
-alfabeto_m = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-numeros=['0','1','2','3','4','5','6','7','8','9']
-reservadas= ['program','procedure','var','int','float','boolean','read','write','true','false','begin','end','if','then','else','while','do','or','div','and','not']
-simbolos = ['::=',':=',':','=','<','>','<=','>=',',','<>','+','-','*','[',']','{','}',';','.','_','(',')','/']
+def program(index):
+    token,i_lexemas = obter_token(index)
+    if token == 'PALAVRA RESERVADA PROGRAM':
+        token,i_lexemas = obter_token(i_lexemas)
+        if token == 'IDENTIFICADOR':
+            token,i_lexemas = obter_token(i_lexemas)
+            if token == 'OP_PONTO_VIRGULA':
+                bloco_(i_lexemas)
+            else:
+                sintatico.insert(END,'Falta o ;')
+                bloco_(i_lexemas-1)
+        else:
+            sintatico.insert(END,'Falta o Identificador')
+            bloco_(i_lexemas)
+    else:
+        sintatico.insert(END,'Falta o "program" ')
+        bloco_(i_lexemas+1)
+
+def bloco_(index):
+    token,i_lexemas = obter_token(index)
+    if token == 'PALAVRA RESERVADA INT' or token == 'PALAVRA RESERVADA BOOLEAN':
+        i_lexemas = parte_declaracao_variaveis(i_lexemas-1)
+    if token == 'PALAVRA RESERVADA PROCEDURE':
+        a=0
+    return i_lexemas
+
+
+
+def lista_de_identificadores(index):
+    token,i_lexemas = obter_token(index)
+    if token == 'IDENTIFICADOR':
+        token,i_lexemas = obter_token(i_lexemas)
+        if token == 'OP_VIRGULA' or token == 'IDENTIFICADOR':
+            while True:
+                if token == 'OP_VIRGULA':
+                    token,i_lexemas = obter_token(i_lexemas)
+                    if token == 'IDENTIFICADOR':
+                        token,i_lexemas = obter_token(i_lexemas)
+                        if token != 'OP_VIRGULA' and token != 'IDENTIFICADOR':
+                            break
+                    elif token == 'OP_VIRGULA':
+                        sintatico.insert(END,'Falta um Identificador entre as virgulas')
+                        token,i_lexemas = obter_token(i_lexemas)
+                    else:
+                        sintatico.insert(END,'Nao terminou com um identificador')
+                        break
+                elif token == 'IDENTIFICADOR':
+                    token2,i_lexemas2 = obter_token(i_lexemas-2)
+                    if token2 == 'OP_VIRGULA':
+                        token, i_lexemas = obter_token(i_lexemas)
+                    else:
+                        sintatico.insert(END,'Falta uma virgula entre os identificadores')
+                        token,i_lexemas = obter_token(i_lexemas)
+
+                    if token != 'OP_VIRGULA' and token != 'IDENTIFICADOR':
+                        break
+    return i_lexemas
+
+def declaracao_variaveis(index):
+    token,i_lexemas = obter_token(index)
+    if token == 'PALAVRA RESERVADA INT' or token == 'PALAVRA RESERVADA BOOLEAN':
+        i_lexemas = lista_de_identificadores(i_lexemas)
+    else:
+        sintatico.insert(END,'tipo nao suportado')
+    return i_lexemas
+
+def parte_declaracao_variaveis(index):
+    i_lexemas = declaracao_variaveis(index)
+    token,i_lexemas = obter_token(i_lexemas-1)
+    while True:
+        if token == 'OP_PONTO_VIRGULA':
+            i_lexemas = declaracao_variaveis(i_lexemas)
+            token,i_lexemas = obter_token(i_lexemas-1)
+            token2,i_lexemas2 = obter_token(i_lexemas)
+            if token2 != 'PALAVRA RESERVADA INT' and token != 'PALAVRA RESERVADA BOOLEAN':
+                break
+    return i_lexemas
+                
 
 #------------------------------------------ INTERFACE ------------------------------------------------------------------
 
 janela = Tk()
 janela.title("Analisador Lexico")
-janela.geometry("1000x400")
+janela.geometry("1000x450")
 janela.config(background='#BA55D3')
 
 scrollbar_l = Scrollbar(janela,orient='vertical')
@@ -403,13 +535,13 @@ tabela2.grid(column=4,row=0,padx=0,pady=0)
 tabela3 = Listbox(janela,height=20,width=10,borderwidth=2,yscrollcommand = scrollbar.set)
 tabela3.grid(column=5,row=0,padx=0,pady=0)
 
-tabela.bind('<MouseWheel>', scrollwheel)
-tabela2.bind('<MouseWheel>', scrollwheel)
-tabela3.bind('<MouseWheel>', scrollwheel)
 
 scrollbar.config( command = multiple_yview )
 botao = Button(janela, text="Analisar", command = imprimirAnalise)
 botao.grid(column=3, row=1, padx=0, pady=5)
+
+sintatico = Listbox(janela,height=5,width=80,borderwidth=2,yscrollcommand = scrollbar.set)
+sintatico.grid(column=2,row=1,padx=0,pady=5)
 
 menu = Menu(janela)
 menu_arq = Menu(menu)
